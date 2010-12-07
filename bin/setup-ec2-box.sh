aaddaa9439f3a8781010212dc4b6d81321731d88
Jeffrey S. Haemer <jsh@usenix.org>
2010-12-07 12:36:42 -0700

    Make installed aws/sdk accessible
    Hook it into /usr/share/php/AWSSDKforPHP

--- bin/setup-ec2-box.sh
+++ bin/setup-ec2-box.sh
@@ -4,3 +4,5 @@ yum update -y
 yum install git php-pear
 pear channel-discover pear.amazonwebservices.com
 pear install aws/sdk
+git clone git://github.com/jsh/BarrBookCode.git
+ln -s /usr/share/pear/AWSSDKforPHP/ /usr/share/php/AWSSDKforPHP
