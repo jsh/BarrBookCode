cc3c7b384494c46f14cd1bd0151c603c17a65600
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2010-12-10 16:46:13 -0700

    Mark modified and Cloudfusion->AWSSDKforPHP

--- code/chapter_06/load_crawl_urls.php
+++ code/chapter_06/load_crawl_urls.php
@@ -19,11 +19,13 @@
  * OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the
  * License.
+ *
+ * Modified by Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
  */
 
 error_reporting(E_ALL);
 
-require_once('cloudfusion.class.php');
+require_once('AWSSDKforPHP/sdk.class.php');
 require_once('include/book.inc.php');
 
 // Make sure that at least one argument was given
@@ -59,3 +61,4 @@ for ($i = 1; $i < $argc; $i++)
   }
 }
 ?>
+
