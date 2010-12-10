94c2086b141825bbf40b91e18ec3752afcdd38c2
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2010-12-10 16:31:56 -0700

    First step conversion.
    Add a modified-by string and change Cloudfusion->AWSSDKforPHP

--- code/chapter_06/list_all_queues.php
+++ code/chapter_06/list_all_queues.php
@@ -19,6 +19,8 @@
  * OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the
  * License.
+ *
+ * Modified by Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
  */
 
 error_reporting(E_ALL);
