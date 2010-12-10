47c1b8ac7464a8e2213eab41316949b534713a8d
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2010-12-09 17:28:05 -0700

    Check for correct usage.

--- bin/cloudfusion-to-AS4P.sh
+++ bin/cloudfusion-to-AS4P.sh
@@ -3,4 +3,9 @@
 # convert from older, cloudfusion toolkit to AWSSDKforPHP
 #   in-place conversion
 
+die() { echo $* >&2; exit 1; }
+usage="usage: $0 phpfile [phpfile ...]"
+
+[ "$*" ] || die $usage
+
 perl -i -pe "s('cloudfusion.class.php')('AWSSDKforPHP/sdk.class.php')" $*
