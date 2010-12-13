89a17d23b7fddcf56b57c44c1e1986642673f968
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2010-12-13 14:23:33 -0700

    Conversion to AWSSDKforPHP.

--- code/chapter_07/statistics_chart_page.php
+++ code/chapter_07/statistics_chart_page.php
@@ -25,11 +25,13 @@
  * OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the
  * License.
+ *
+ * Modified by Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
  */
 
 error_reporting(E_ALL);
 
-require_once('cloudfusion.class.php');
+require_once('AWSSDKforPHP/sdk.class.php');
 
 // Set default date values
 $startDate_DT = new DateTime('now');
