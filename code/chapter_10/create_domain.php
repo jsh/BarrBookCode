b437b6c1d03821d63e9425cbaaa82395d690a0d2
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2010-12-24 08:51:48 -0700

    Cloudwatch->AWSSDKforPHP

--- code/chapter_10/create_domain.php
+++ code/chapter_10/create_domain.php
@@ -19,11 +19,13 @@
  * OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the
  * License.
+ *
+ * Modified by Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
  */
 
 error_reporting(E_ALL);
 
-require_once('cloudfusion.class.php');
+require_once('AWSSDKforPHP/sdk.class.php');
 require_once('include/book.inc.php');
 
 $domain = BOOK_SNAP_LOG_DOMAIN;
--- code/chapter_10/ec2_diagram.php
+++ code/chapter_10/ec2_diagram.php
@@ -22,10 +22,12 @@
  * OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the
  * License.
+ *
+ * Modified by Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
  */
 
 error_reporting(E_ALL);
-require_once('cloudfusion.class.php');
+require_once('AWSSDKforPHP/sdk.class.php');
 require_once('include/book.inc.php');
 
 // Define shape geometry
