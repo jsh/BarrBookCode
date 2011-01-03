08d38ebd266365571201db72e2ac04f94d1d8ae9
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2011-01-03 16:39:27 -0700

    Lint production for quick syntax checks.

--- make-modules/bbc.mk
+++ make-modules/bbc.mk
@@ -1,8 +1,9 @@
 # include this with
 # include ../../make-modules/bbc.mk
 
-SHELL := /bin/bash
+SHELL     := /bin/bash
 
+## cleanliness is next to godliness
 cleanfiles := Notes.{html,txt}
 
 clean:
@@ -11,9 +12,22 @@ clean:
 distclean:
 	git -d -x clean
 
+## make local html and text copies of documentation from Google Docs
+
 doc:
 	curl $(docurl) > Notes.html 2>/dev/null
 	w3m -dump Notes.html | perl -pe 's/^(\s+)1\./\1+/' > Notes.txt
 
-.PHONY: clean doc
+
+## set up for linting
+
+phpfiles  := $(wildcard *.php */*.php)
+lint_tgts := $(phpfiles:.php=.lint)
+
+%.lint:%.php
+	php -l $<
+
+lint: $(lint_tgts)
+
+.PHONY: clean distclean doc lint
 
