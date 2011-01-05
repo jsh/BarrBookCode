8589bfd11554dd4e0b0239cfe5ccdd2ba9b2dd2f
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2011-01-04 17:07:27 -0700

    comments

--- bin/cloudfusion-to-AS4P.sh
+++ bin/cloudfusion-to-AS4P.sh
@@ -8,7 +8,10 @@ usage="usage: $0 phpfile [phpfile ...]"
 
 [ "$*" ] || die $usage
 
+# point at the new toolkit
 perl -i -pe "s('cloudfusion.class.php')('AWSSDKforPHP/sdk.class.php')" $*
+
+# mark as modified, for Apache license compliance
 for i in $*; do
   mark-modified.pl $i > $i.tmp &&
     cp $i.tmp $i &&
